76f26c89f189a6c1ace749980fcc8c925f31da19
imransolanki <imran.b.solanki@gmail.com>
2016-08-20 19:10:59 +0530

    Committing Layout manager demo which has sample layout's demonstrating LayoutManager-Linear,Relative with login screen, calculator etc

---
+++ LayoutManagers/app/src/main/java/com/example/imran/layoutmanagers/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.imran.layoutmanagers;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.inner_layouts);
+    }
+}
